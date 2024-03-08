e60a4bfdc881bc3c0782e417fe4f75900ec3ff62
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-08 11:54:24 +0400

    added functions to find default interface and generate wireguard keypair

--- src/tenminutevpn.bash
+++ src/tenminutevpn.bash
@@ -4,3 +4,16 @@ set -e -o pipefail
 
 TENMINUTEVPN_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
 export PATH="$(dirname "$TENMINUTEVPN_PATH"):$PATH"
+
+# function that finds default interface
+find_default_interface() {
+    ip route | awk '/^default/ {print $5}'
+}
+
+
+# function that generates a new wireguard keypair in target directory
+wireguard_keygen() {
+    local target_dir="$1"
+    mkdir -p "$target_dir"
+    wg genkey | tee "$target_dir/privatekey" | wg pubkey > "$target_dir/publickey"
+}
