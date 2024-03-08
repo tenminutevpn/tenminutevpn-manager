4ddfeda1bf30a281057a4261fb0bd5f95f2a6ace
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-08 12:03:50 +0400

    removed source-path from shellcheckrc, renamed functions and added new functions for ip detection and wireguard config generation in tenminutevpn.bash

--- src/tenminutevpn.bash
+++ src/tenminutevpn.bash
@@ -6,14 +6,30 @@ TENMINUTEVPN_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
 export PATH="$(dirname "$TENMINUTEVPN_PATH"):$PATH"
 
 # function that finds default interface
-find_default_interface() {
+network_interface_default() {
     ip route | awk '/^default/ {print $5}'
 }
 
+# function that finds public ipv4 address
+network_ipv4_public() {
+    curl -s https://ipinfo.io/ip
+}
+
+# function that finds private ipv4 address
+network_ipv4_private() {
+    local interface="$1"
+    ip addr show dev "$interface" | awk '/inet / {print $2}' | cut -d/ -f1
+}
 
 # function that generates a new wireguard keypair in target directory
-wireguard_keygen() {
+wireguard_generate_keypair() {
     local target_dir="$1"
     mkdir -p "$target_dir"
     wg genkey | tee "$target_dir/privatekey" | wg pubkey > "$target_dir/publickey"
+    chmod 600 "$target_dir/privatekey"
+}
+
+# function that generates a new wireguard configuration file
+wireguard_generate_config() {
+    true;
 }
