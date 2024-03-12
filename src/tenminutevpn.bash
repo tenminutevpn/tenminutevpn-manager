2c3843f350d08297828f5cb9faf82e2ab722d27c
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-12 20:46:09 +0400

    added directory creation for wireguard, modified client config generation and added peer

--- src/tenminutevpn.bash
+++ src/tenminutevpn.bash
@@ -53,6 +53,7 @@ wireguard_generate_server_config() {
     local interface="$1"
     local privatekey="$2"
 
+    mkdir -p "/etc/wireguard"
     cat <<EOF > "/etc/wireguard/$WIREGUARD_INTERFACE.conf"
 [Interface]
 PrivateKey = $privatekey
@@ -132,7 +133,11 @@ wireguard_setup() {
 
     wireguard_generate_keypair "/etc/wireguard"
     wireguard_generate_server_config "$interface" "$(cat /etc/wireguard/privatekey)"
-    wireguard_generate_client_config "$interface" "$(cat /etc/wireguard/privatekey)" "$(cat /etc/wireguard/publickey)" "$(network_ipv4_public)"
+
+    wireguard_generate_keypair "${WIREGUARD_PEERS_PATH}/client-1"
+    wireguard_generate_client_config "$interface" "$(cat "${WIREGUARD_PEERS_PATH}/client-1/privatekey")" "$(cat /etc/wireguard/publickey)" "$(network_ipv4_public):$WIREGUARD_PORT"
+    wireguard_add_peer "${WIREGUARD_INTERFACE}" "$(cat "${WIREGUARD_PEERS_PATH}/client-1/publickey")" "100.96.0.2/32"
+
     wireguard_start
 }
 
