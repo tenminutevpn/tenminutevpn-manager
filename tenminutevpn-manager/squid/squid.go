9a6226b58685e2ea9dd4c5cebc266d66767df7b0
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-18 22:38:36 +0400

    added squid package with systemd service and template rendering functionality

---
+++ tenminutevpn-manager/squid/squid.go
@@ -0,0 +1,21 @@
+package squid
+
+import (
+	"github.com/tenminutevpn/tenminutevpn-manager/systemd"
+)
+
+type Squid struct {
+	Port int
+}
+
+func NewSquid(port int) *Squid {
+	return &Squid{Port: port}
+}
+
+func (s *Squid) SystemdService() *systemd.Service {
+	return systemd.NewService("squid")
+}
+
+func (s *Squid) Render() string {
+	return makeTemplateSquidData(s).Render()
+}
---
+++ tenminutevpn-manager/squid/templates.go
@@ -0,0 +1,43 @@
+package squid
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"io/fs"
+	"strings"
+)
+
+//go:embed templates
+var (
+	templates     embed.FS
+	templateSquid *template.Template
+)
+
+func getTemplate(name string) *template.Template {
+	tpl, err := fs.ReadFile(templates, fmt.Sprintf("templates/%s.tpl", name))
+	if err != nil {
+		panic(fmt.Errorf("failed to read template %s: %w", name, err))
+	}
+	return template.Must(template.New(name).Parse(string(tpl)))
+}
+
+func init() {
+	templateSquid = getTemplate("squid.conf")
+}
+
+type templateSquidData struct {
+	Port int
+}
+
+func makeTemplateSquidData(s *Squid) *templateSquidData {
+	return &templateSquidData{
+		Port: s.Port,
+	}
+}
+
+func (t *templateSquidData) Render() string {
+	var output strings.Builder
+	templateSquid.Execute(&output, t)
+	return output.String()
+}
