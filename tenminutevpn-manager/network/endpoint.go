f4e29545ddfa11f684d398a889a40c1bd41d92bf
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-20 22:44:18 +0400

    added endpoint struct and yaml unmarshal functionality in network package

---
+++ tenminutevpn-manager/network/endpoint.go
@@ -0,0 +1,46 @@
+package network
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+type Endpoint struct {
+	IP   net.IP
+	Port int
+}
+
+func (endpoint *Endpoint) String() string {
+	return net.JoinHostPort(endpoint.IP.String(), fmt.Sprintf("%d", endpoint.Port))
+}
+
+func (endpoint *Endpoint) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var endpointStr string
+	if err := unmarshal(&endpointStr); err != nil {
+		return err
+	}
+
+	host, portStr, err := net.SplitHostPort(endpointStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse endpoint: %w", err)
+	}
+
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return fmt.Errorf("invalid ip address: %s", host)
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse port: %w", err)
+	}
+
+	if port < 0 || port > 65535 {
+		return fmt.Errorf("invalid port: %d", port)
+	}
+
+	endpoint.IP = ip
+	endpoint.Port = port
+	return nil
+}
